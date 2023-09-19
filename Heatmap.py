ed4aff00beb5dd10f546f2753d6618b2f257933c
PHILIP KIBET <89591958+Apollop24@users.noreply.github.com>
2023-09-19 16:01:26 +0300

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,153 +1,42 @@
-#install the required libraries
-
-#pip install pandas
-#pip install numpy
-#pip install matplotlib
-#pip install seaborn
-#pip install geopandas matplotlib descartes
-
-
-
-#import the libraries
+# Install required libraries 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import geopandas as gpd
 
-# Specify the path to the dat in Excel file
-csv_file_path = r"C:\Users\TOO\Downloads\HEATMAP\sales_record.csv"
+# Load data
+# Specify path to CSV file
+csv_file_path = r"C:\Users\TOO\Downloads\HEATMAP\sales_record.csv" 
 
-# Read the Excel file into a pandas DataFrame
+# Read CSV into pandas DataFrame
 data = pd.read_csv(csv_file_path)
 
-# View the imported data (optional)
-print(data)
-
-
-
-# Group the data by 'Country' and calculate the total profit for each country
+# Group data by 'Country' and calculate total profit 
 profit_by_country = data.groupby('Country')['Total Profit'].sum().reset_index()
-print(profit_by_country )
 
-# Load the world countries shapefile
-
+# Load world countries shapefile
 world = gpd.read_file('C:/Users/TOO/Downloads/WORLD/ne_50m_admin_0_countries.shp')
-# Merge the profit_by_country DataFrame with the world shapefile based on the country name
+
+# Merge profit data with world shapefile on country name
 merged_data = world.merge(profit_by_country, left_on='ADMIN', right_on='Country', how='left')
 
-# Plot the geographical heatmap
+# Create geographic heatmap 
 fig, ax = plt.subplots(figsize=(12, 8))
-
-# Ensure 'Total Profit' values are numeric (replace 'Total Profit' with the actual column name if needed)
-merged_data['Total Profit'] = pd.to_numeric(merged_data['Total Profit'], errors='coerce')
-
-# Plot the heatmap using 'Total Profit' column
+merged_data['Total Profit'] = pd.to_numeric(merged_data['Total Profit'], errors='coerce') 
 merged_data.plot(column='Total Profit', cmap='coolwarm', linewidth=0.8, ax=ax, legend=True, legend_kwds={'label': 'Total Profit'})
 
-# Add a title to the plot
-plt.title('Geographical Heatmap of Total Profit by Country')
+# Add plot title and remove axes
+plt.title('Geographical Heatmap of Total Profit by Country')  
+ax.set_axis_off() 
 
-# Remove axis
-ax.set_axis_off()
-
-# Show the plot
+# Show plot
 plt.show()
 
+# Create sample correlation matrix
+correlation_matrix = np.array([[1.0, 0.8, 0.6], [0.8, 1.0, 0.4], [0.6, 0.4, 1.0]])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Sample correlation matrix
-correlation_matrix = np.array([[1.0, 0.8, 0.6],
-                               [0.8, 1.0, 0.4],
-                               [0.6, 0.4, 1.0]])
-
-# Create the heatmap
+# Create correlation heatmap 
 plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
 plt.title("Correlation Heatmap")
